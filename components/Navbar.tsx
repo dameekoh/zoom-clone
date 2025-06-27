2136a63da34c2912fd0f601c8ba4c7b8c469488c
Damir <adidukik@gmail.com>
2025-06-28 07:49:38 +0900

    refactor utilities

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
           alt="zoom"
           width={32}
           height={32}
-          className="max-sm:hidden"
+          className="max-sm:size-10"
         />
         <p className="text-[26px] font-extrabold text-white max-sm:hidden">
           Yoom
