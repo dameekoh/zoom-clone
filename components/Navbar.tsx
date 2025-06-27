d1353eccda29497005e3d48d9434919ff8c493f4
Damir <adidukik@gmail.com>
2025-06-28 07:00:49 +0900

    add navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
+import MobileNav from "@/components/MobileNav";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="zoom"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Yoom
+        </p>
+      </Link>
+
+      <div className="flex-between gap-5">
+        {/* <SignedIn>
+          <UserButton afterSignOutUrl="/sign-in" />
+        </SignedIn> */}
+
+        <MobileNav />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
